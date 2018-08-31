ff549cd610f753f34f7e3f2b846f3d8349304544
Simon Gordon <gordonsimon@hotmail.com>
2018-08-31 21:16:54 +1000

    Build list of notes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,55 @@
 import * as React from "react";
+import { Formik, Form, Field } from "formik";
+
+interface Note {
+    content: string;
+}
 
 interface AppProps {}
-interface AppState {}
+interface AppState {
+    notes: Note[]
+}
 
 class App extends React.Component<AppProps, AppState> {
     constructor(props: AppProps) {
         super(props);
+        this.state = {
+            notes: []
+        }
+    }
+
+    addNote(newNote: Note) {
+        this.setState(({notes}) => {
+            notes.push(newNote);
+            return ({notes});
+        })
     }
 
     render() {
-        return <div>This is the app</div>;
+        const {notes} = this.state;
+        return <div>
+            <ul>
+            {
+                notes.map(n => <li>{n.content}</li>)
+            }
+            </ul>
+            <Formik
+                initialValues={{
+                    content: ''
+                }}
+                onSubmit={(values, {resetForm}) => {
+                    this.addNote(values);
+                    resetForm();
+                }}
+                render={({values}) => <Form>
+                        <Field 
+                        id="content"
+                        name="content"
+                        />
+                        <button type="submit">Add</button>
+                    </Form>}
+            />
+        </div>;
     }
 }
 
