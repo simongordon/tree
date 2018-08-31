7ed9ff0ecb5fbf8245ab6274d1025d78b5fee350
Simon Gordon <gordonsimon@hotmail.com>
2018-08-31 21:40:56 +1000

    Add vis to project

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Formik, Form, Field } from "formik";
+import Graph from "vis-react";
 
 interface Note {
     content: string;
@@ -9,6 +10,39 @@ interface AppProps {}
 interface AppState {
     notes: Note[]
 }
+
+var graph = {
+    nodes: [
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+      ],
+    edges: [
+        {from: 1, to: 2},
+        {from: 1, to: 3},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+      ]
+  };
+   
+  var options = {
+    //   width: "100px",
+    //   height: "100px",
+      layout: {
+          hierarchical: true
+      },
+      edges: {
+          color: "#000000"
+      }
+  };
+   
+  var events = {
+      select: function(event) {
+          var { nodes, edges } = event;
+      }
+  }
 
 class App extends React.Component<AppProps, AppState> {
     constructor(props: AppProps) {
@@ -33,6 +67,7 @@ class App extends React.Component<AppProps, AppState> {
                 notes.map(n => <li>{n.content}</li>)
             }
             </ul>
+
             <Formik
                 initialValues={{
                     content: ''
@@ -49,6 +84,10 @@ class App extends React.Component<AppProps, AppState> {
                         <button type="submit">Add</button>
                     </Form>}
             />
+                        <div>
+
+<Graph graph={graph} options={options} events={events} />
+</div>
         </div>;
     }
 }
