c6c596269fafa6505f5f7ed8ab3a93002b24eb62
Simon Gordon <gordonsimon@hotmail.com>
2018-08-31 20:56:03 +1000

    Extract component

---
+++ ui/src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const App = () => <div>This is the app</div>
+
+export default App;
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
-const App = () => <div>This is the app</div>
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById("main"));
